a82426c9ec1322c6df951a7d2d0d7c1207ae0c55
Haaris Iqubal <mdhaarisiqubal@icloud.com>
2021-05-21 23:20:12 +0530

    App Completed

--- Expandable Views/ContentView.swift
+++ Expandable Views/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items = DirectoryData.items
+    @State private var textField = ""
+    @State private var textField2 = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List{
+                Section(header:Text("Outline View")){
+                    ForEach(items){item in
+                        DisclosureGroup("\(Image(item.icon)) \(item.name)"){
+                        OutlineGroup(item.children ?? [Directory](), children:\.children){child in
+                            if child.children == nil{
+                                NavigationLink(destination: Text(child.name)){
+                                    Image(systemName: child.icon)
+                                    Text(child.name)
+                                }
+                            }
+                            else{
+                                Label(child.name, systemImage:child.icon)
+                            }
+
+                            }
+                        }
+                    }
+                }
+                
+                Section(header:Text("Disclosure View")){
+                    DisclosureGroup("Settings Form"){
+                        Section(header: Text("Personal Info")){
+                            TextField("Enter your name", text: $textField)
+                            TextField("Enter your country",text: $textField2)
+                        }
+                        Section(header: Text("Preference")){
+                            Toggle("Dark Mode", isOn:.constant(true))
+                            Toggle("Notification", isOn:.constant(false))
+                        }
+                    }
+                }
+                
+            }
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("Expandable Views")
+        }
     }
 }
 
